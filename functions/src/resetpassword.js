0344949b25ad590575464a5105d5cb50f34d830c
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-11-26 01:14:19 +0100

    Update resetpassword.js

--- functions/src/resetpassword.js
+++ functions/src/resetpassword.js
@@ -1,13 +1,9 @@
 const admin = require("firebase-admin");
 const logger = require("firebase-functions/logger");
-const handlebars = require("handlebars");
+const {
+  sendEmail,
+} = require("./shared/emailSender");
 const path = require("path");
-const {
-  readHTMLFile,
-} = require("./shared/readHTMLFile");
-const {
-  transporter,
-} = require("./shared/emailTransporter");
 
 const templatePath = path.join(__dirname, "../tpl/reset_password.html");
 
@@ -43,36 +39,18 @@ exports.resetPassword = async function(req, res) {
 
     await admin.auth().generatePasswordResetLink(email)
         .then((link) => {
-          readHTMLFile(templatePath, (error, html) => {
-            if (error) {
-              logger.error("Error reading template file:", error);
-              return res.status(500).json({
-                error: "Password reset error",
-              });
-            }
-            const template = handlebars.compile(html);
-            const replacements = {
+          const mailOptions = {
+            from: "no-reply@readthevoice.com",
+            to: email,
+            subject: "Reset your password",
+            replacements: {
               email: email,
               firstName: firstName,
               lastName: lastName,
               link: link,
-            };
-            const htmlToSend = template(replacements);
-            const mailOptions = {
-              from: "no-reply@readthevoice.com",
-              to: email,
-              subject: "Reset your password",
-              html: htmlToSend,
-            };
-            transporter.sendMail(mailOptions, (error, response) => {
-              if (error) {
-                logger.error("Error when sending email:", error);
-                return res.status(500).json({
-                  error: "Password reset error",
-                });
-              }
-            });
-          });
+            },
+          };
+          sendEmail(templatePath, mailOptions, res, "Password reset error");
         })
         .catch((error) => {
           logger.error("Error with password reset link:", error);
